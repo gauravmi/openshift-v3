104520d05e5e27148e80876c4bdbf0bb4d347b49
Gaurav <gmi3024@gmail.com>
2017-05-27 15:58:59 +0530

    refactoring paths

--- spec/config_reader_spec.rb
+++ spec/config_reader_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
-PROJECT_ROOT_PATH = File.join(__dir__, '..')
-PROJECT_CONFIG_PATH = File.join(PROJECT_ROOT_PATH, 'config')
-PROJECT_LIB_PATH = File.join(PROJECT_ROOT_PATH, 'lib')
+PROJECT_ROOT_PATH = Pathname(__dir__).parent
+PROJECT_CONFIG_PATH = PROJECT_ROOT_PATH + 'config'
+PROJECT_LIB_PATH = PROJECT_ROOT_PATH + 'lib'
 
 load File.join(PROJECT_LIB_PATH, 'config_reader.rb')
 
 describe 'config reader'  do
 	it 'should read config' do
-		config_reader = ConfigReader.new(File.join(PROJECT_CONFIG_PATH, 'config.yml')).read
-		expect(config_reader.username).to eq('gmi3024@gmail.com')
+		config_file_path = PROJECT_CONFIG_PATH + 'config.yml'
+		config_reader = ConfigReader.new(config_file_path).read
 		expect(config_reader.url).to eq('https://console.starter-us-east-1.openshift.com/oapi/v1')
 	end
 end
